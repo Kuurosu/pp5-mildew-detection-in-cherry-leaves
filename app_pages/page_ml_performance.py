eb120fe5a119ed393012f4f1b6d4a7dc1b4c32d4
Rob Sizeland <87565795+Kuurosu@users.noreply.github.com>
2023-07-08 23:52:01 +0100

    Add the model history images to ml_performance_metrics

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -9,3 +9,14 @@ def page_ml_performance_metrics():
   labels_distribution = plt.imread(f"outputs/{version}/label_distribution.png")
   st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
   st.write("---")
+  
+  st.write("### Model History")
+  col1, col2 = st.columns(2)
+  with col1: 
+    model_acc = plt.imread(f"outputs/{version}/model_training_acc.png")
+    st.image(model_acc, caption='Model Training Accuracy')
+  with col2:
+    model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+    st.image(model_loss, caption='Model Training Losses')
+  st.write("---")
+  
