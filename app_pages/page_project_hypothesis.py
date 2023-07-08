5b8ff9b24562f35139a73478418a5d3bd02e594f
Rob Sizeland <87565795+Kuurosu@users.noreply.github.com>
2023-07-08 23:30:48 +0100

    Add introduction to the hypothesis body

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -1,4 +1,13 @@
 import streamlit as st
 
 def page_project_hypothesis_body():
-  st.write("yo")
+
+  st.write("### Hypothesis 1 and Validation")
+
+  st.success(
+    f"Infected leaves possess noticeable and unique features that clearly \n"
+    f"differentiate them from healthy leaves, enabling easy visual \n"
+    f"identification."
+  )
+  
+  
