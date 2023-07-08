8211464cca5970fdcb4d6dcf120cec0395b05059
Rob Sizeland <87565795+Kuurosu@users.noreply.github.com>
2023-07-08 23:44:48 +0100

    Add the machine learning performance metrics page

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def page_project_hypothesis_body():
 
-  st.write("### Hypothesis 1 and Validation")
+  st.write("### Hypothesis and Validation")
 
   st.success(
     f"Infected leaves possess noticeable and unique features that clearly \n"
