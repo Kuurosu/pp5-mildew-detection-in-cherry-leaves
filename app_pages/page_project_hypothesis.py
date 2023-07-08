93f7d28935ebdd02983bc0a0fb2606a3edc1951e
Rob Sizeland <87565795+Kuurosu@users.noreply.github.com>
2023-07-08 23:38:00 +0100

    Add the proof for hypothesis 1

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -15,3 +15,10 @@ def page_project_hypothesis_body():
     f"Typically, the initial indication is the presence of a light-green, circular lesion on the leaf's surface, "
     f"followed by the emergence of a subtle white cotton-like growth in the affected region."
     )
+  
+  st.warning(
+    f"The model has successfully discerned these distinctions and acquired the capacity to differentiate and generalise, resulting in precise predictions."
+    f" A robust model possesses the capability to make predictions on new data beyond the training set, without relying heavily on specific instances."
+    f" By capturing the underlying patterns between features and labels rather than memorising the exact relationships within the training dataset,"
+    f" the model can reliably infer future observations based on the general patterns observed."
+    )
