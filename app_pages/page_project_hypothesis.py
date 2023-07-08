5ee5038f7ffd04198037a8764b089442342d4f39
Rob Sizeland <87565795+Kuurosu@users.noreply.github.com>
2023-07-08 23:33:20 +0100

    Add another paragraph to the hypothesis discussing our reasoning behind the decision of which leaves are infected or not

--- app_pages/page_project_hypothesis.py
+++ app_pages/page_project_hypothesis.py
@@ -8,6 +8,10 @@ def page_project_hypothesis_body():
     f"Infected leaves possess noticeable and unique features that clearly \n"
     f"differentiate them from healthy leaves, enabling easy visual \n"
     f"identification."
-  )
+    )
   
-  
+  st.info(
+    f"We hypothesise that cherry leaves affected by powdery mildew exhibit distinctive characteristics. "
+    f"Typically, the initial indication is the presence of a light-green, circular lesion on the leaf's surface, "
+    f"followed by the emergence of a subtle white cotton-like growth in the affected region."
+    )
