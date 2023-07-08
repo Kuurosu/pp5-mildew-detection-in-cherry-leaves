899f0e043770c6846caeff736cd513c496fa8e18
Rob Sizeland <87565795+Kuurosu@users.noreply.github.com>
2023-07-08 21:53:18 +0100

    Create the introduction for the leaves_visualiser

--- app_pages/page_leaves_visualiser.py
+++ app_pages/page_leaves_visualiser.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
 def page_leaves_visualiser_body():
-  st.write("yo")
+  st.info(
+    f"A study was conducted to visually differentiate cherry leaves affected by powdery mildew "
+    f"from healthy ones. The goal was to develop a reliable method for accurately identifying "
+    f"powdery mildew presence on cherry leaves, aiding in effective disease detection and management."
+    )
+  
