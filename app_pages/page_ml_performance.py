61f2ffe7aa6ddde969d5bd110dd9377967e5333f
Rob Sizeland <87565795+Kuurosu@users.noreply.github.com>
2023-07-08 23:49:31 +0100

    Add the labels frequencies image to the ml_performance_metrics

--- app_pages/page_ml_performance.py
+++ app_pages/page_ml_performance.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
 def page_ml_performance_metrics():
-  st.write("yoo")
+  version = 'v1'
+
+  st.write("### Train, Validation and Test Set: Labels Frequencies")
+
+  labels_distribution = plt.imread(f"outputs/{version}/label_distribution.png")
+  st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
+  st.write("---")
