e5d7c1a78f486f8a294d8f09c6e7dc852c366293
Rob Sizeland <87565795+Kuurosu@users.noreply.github.com>
2023-07-08 21:38:45 +0100

    Add another paragraph explaining what the project consists of, such as images and dataset forms

--- app_pages/page_summary.py
+++ app_pages/page_summary.py
@@ -20,4 +20,17 @@ def page_summary_body():
         f"powdery mildew in cherry trees."
         )
 
-    
+    st.warning(
+        f"**Project Dataset**\n\n"
+        f"The project dataset comprises a collection of meticulously captured "
+        f"images of cherry leaves. It consists of 2104 images of healthy leaves "
+        f"and an equal number of images depicting leaves affected by powdery "
+        f"mildew. Each leaf was individually photographed against a neutral "
+        f"background to ensure accurate representation.\n\n"
+        f"This comprehensive dataset forms the foundation for our analysis and "
+        f"model development, enabling us to explore the visual characteristics "
+        f"and patterns associated with healthy and affected leaves. By leveraging "
+        f"this dataset, we aim to develop a robust model for powdery mildew "
+        f"detection in cherry trees, contributing to effective disease management "
+        f"strategies and crop protection measures."
+    )
